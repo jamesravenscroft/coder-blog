11a2944a82cebafa2efe93ddbfb5d267bd5ad1e6
James Ravenscroft <hello@ravenscroftgames.net>
2019-12-26 16:27:25 -0800

    initial commit

---
+++ src/build/precache-manifest.8709e8915ceda493eaae57a085e17ff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88b5d092038407d495d610581917371",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97e12a558c4273536b8c",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "fa5810c285fb16dbabc8",
+    "url": "/static/js/2.3add966f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.3add966f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97e12a558c4273536b8c",
+    "url": "/static/js/main.a414fe29.chunk.js"
+  },
+  {
+    "revision": "c48705f12e5c765b8797",
+    "url": "/static/js/runtime-main.db0a3d0d.js"
+  }
+]);
